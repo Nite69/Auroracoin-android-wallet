700b02d6d0dbd9887ed6cf4505b0387633a94248
nite69 <hko@cave.(none)>
2014-02-23 09:21:47 +0200

    Committer: nite69 	modified:   wallet/src/de/schildbach/wallet/Constants.java

--- wallet/src/de/schildbach/wallet/Constants.java
+++ wallet/src/de/schildbach/wallet/Constants.java
@@ -88,7 +88,7 @@ public class Constants
 	public static final int MBTC_MAX_PRECISION = 5;
 	public static final int LOCAL_PRECISION = 4;
 
-	public static final String DONATION_ADDRESS = "AKFhny5tdJSTiSBRgBzpQzE5e66tEA3j1T";
+	public static final String DONATION_ADDRESS = "AXVoGgYtSVkPv96JLL7CiwcyVvPxXHXRK9";
 	public static final String REPORT_EMAIL = "dev@litecoin.org";
 	public static final String REPORT_SUBJECT_ISSUE = "Reported issue";
 	public static final String REPORT_SUBJECT_CRASH = "Crash report";
