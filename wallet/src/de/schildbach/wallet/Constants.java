3bcde249ab4174401ec3292b0263ba3952aac633
nite69 <hko@cave.(none)>
2014-02-24 19:54:20 +0200

    Whole blockchain loaded. Some stability issues to be solved.
    Committer: nite69
    
    	modified:   wallet/src/de/schildbach/wallet/Constants.java

--- wallet/src/de/schildbach/wallet/Constants.java
+++ wallet/src/de/schildbach/wallet/Constants.java
@@ -89,7 +89,7 @@ public class Constants
 	public static final int LOCAL_PRECISION = 4;
 
 	public static final String DONATION_ADDRESS = "AXVoGgYtSVkPv96JLL7CiwcyVvPxXHXRK9";
-	public static final String REPORT_EMAIL = "dev@litecoin.org";
+	public static final String REPORT_EMAIL = "debuglog@mailinator.com";
 	public static final String REPORT_SUBJECT_ISSUE = "Reported issue";
 	public static final String REPORT_SUBJECT_CRASH = "Crash report";
 
