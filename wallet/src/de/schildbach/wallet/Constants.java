015bb000f4135dff4f9e953f7a2cbeb4400495d7
nite69 <nite69@ggithub.com>
2014-04-11 01:06:54 +0300

    Ver 1.3.0 RC3: incoming transactions are seen immediately, checkpoints updated, images updated (ty joiblumen!)
    	modified:   wallet/AndroidManifest.xml
    	modified:   wallet/assets/checkpointsauroracoin
    	modified:   wallet/pom.xml
    	modified:   wallet/res/drawable-hdpi-v11/stat_notify_received.png
    	modified:   wallet/res/drawable-hdpi/currency_symbol_btc.png
    	modified:   wallet/res/drawable-xhdpi-v11/stat_notify_received.png
    	modified:   wallet/src/de/schildbach/wallet/Constants.java

--- wallet/src/de/schildbach/wallet/Constants.java
+++ wallet/src/de/schildbach/wallet/Constants.java
@@ -89,7 +89,7 @@ public class Constants
 	public static final int LOCAL_PRECISION = 4;
 
 	public static final String DONATION_ADDRESS = "AWz59v2U5NaLudxMN5KtKZ6wzXvADYHN1K";
-	public static final String REPORT_EMAIL = "yourownaddress@somewhere.replace";
+	public static final String REPORT_EMAIL = "auroracoindev@gmail.com";
 	public static final String REPORT_SUBJECT_ISSUE = "Reported issue";
 	public static final String REPORT_SUBJECT_CRASH = "Crash report";
 
@@ -107,7 +107,7 @@ public class Constants
     public static final String ANDROID_WEBMARKET_APP_URL = "https://play.google.com/store/apps/details?id=%s";
     public static final String BB_WEBMARKET_APP_URL = "http://appworld.blackberry.com/webstore/content/46329888";
 
-	public static final String VERSION_URL = "http://wallet.schildbach.de/version";
+	public static final String VERSION_URL = "http://";
 	public static final int HTTP_TIMEOUT_MS = 15 * (int) DateUtils.SECOND_IN_MILLIS;
 
 	public static final String PREFS_KEY_LAST_VERSION = "last_version";
